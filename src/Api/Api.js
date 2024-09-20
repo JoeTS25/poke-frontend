5835090dad84cc8507eb9eb7d28d2d1148256d9b
Joe Scibilia <joescibilia@Joes-MacBook-Pro.local>
2024-09-20 13:20:12 -0400

    change url

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:4000";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "https://poke-backend-y5n5.onrender.com";
 
 class PokeApi {
     static token;
